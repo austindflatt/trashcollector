a3faa47c3e3749271bee01a9bd2e25710ed51560
Austin Flatt <austindflatt@gmail.com>
2021-06-04 17:11:46 -0400

    finished customer model, added properties for user stories

--- trash_collector/customers/models.py
+++ trash_collector/customers/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+
 # Create your models here.
 
 # TODO: Finish customer model by adding necessary properties to fulfill user stories
@@ -7,3 +8,10 @@ from django.db import models
 class Customer(models.Model):
     name = models.CharField(max_length=50)
     user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
+    pickup_day = models.CharField(max_length=50, null=True)
+    address = models.CharField(max_length=50, null=True)
+    zip_code = models.CharField(max_length=10, null=True)
+    balance = models.IntegerField(null=True)
+    onetime_pickup = models.DateField(null=True)
+    suspension_start = models.DateField(null=True)
+    suspension_end = models.DateField(null=True)
