13c7435694242e3fec26ef1bc28b014670153a30
Mike Edwards <michael.edwards.4108@gmail.com>
2021-06-07 11:01:36 -0400

    Changed name of 'create' to register for clarity.

--- trash_collector/customers/views.py
+++ trash_collector/customers/views.py
@@ -27,4 +27,4 @@ def create(request):
         new_customer.save()
         return HttpResponseRedirect(reverse('customers:index'))
     else:
-        return render(request, 'customers/create.html')
+        return render(request, 'customers/register.html')
