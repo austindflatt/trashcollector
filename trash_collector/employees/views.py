21696f615e36849f973b73ff04370831a39613d4
Mike Edwards <michael.edwards.4108@gmail.com>
2021-06-10 14:26:02 -0400

    Started create method and associated files to add new employee.

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.apps import apps
 from .models import Employees
-
+from django.urls import reverse
+from django.db.models import Q
 # Create your views here.
 
 # TODO: Create a function for each path created in employees/urls.py. Each will need a template as well.
@@ -18,4 +19,20 @@ def index(request):
             'logged_in_employee': logged_in_employee
         }
     except:
+        return HttpResponseRedirect(reverse('employees:create'))
+    return render(request, 'employees/index.html', context)
+
+
+# Registers an employee's info
+def create(request):
+    if request == "POST":
+        print('Triggered POST')
+        user = request.user
+        name = request.POST.get('name')
+        zipcode = request.POST.get('zipcode')
+        new_employee = Employees(user=user, name=name, zipcode=zipcode)
+        new_employee.save()
+        return HttpResponseRedirect(reverse('employees:index'))
+    else:
+        print('Triggered GET')
         return render(request, 'employees/create.html')
